7cb95dbf322b3af6da62247faf8600003fc22c05
DESKTOP-96Q00G1\Sakis <sthanasis1994@gmail.com>
2020-08-25 13:01:14 +0300

    added view project buttons

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,7 +1,22 @@
 <template>
   <div style="margin-top:50vh">
-    <div class="projectImageBulls"></div>
-    <div class="projectImageSpiderman"></div>
-    <div class="projectImageJuventus"></div>
+    <div class="projectImageBulls">
+      <div class="viewProject">
+        <span>Project 1</span>
+        <button class="btn btn-primary">View Project</button>
+      </div>
+    </div>
+    <div class="projectImageSpiderman">
+      <div class="viewProject">
+        <span>Project 2</span>
+        <button class="btn btn-primary">View Project</button>
+      </div>
+    </div>
+    <div class="projectImageJuventus">
+      <div class="viewProject">
+        <span>Project 3</span>
+        <button class="btn btn-primary">View Project</button>
+      </div>
+    </div>
   </div>
 </template>
