75b6366a123d9f4ef674378aeb2cb30b79ae93b1
DESKTOP-96Q00G1\Sakis <sthanasis1994@gmail.com>
2020-09-10 14:41:00 +0300

    background color added on name

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -6,10 +6,14 @@
       <div id="footerLogo">Graphics</div>
     </div>
     <div class="artistName center fade-in">
-      <div class="name">Alexandros Efthymiou</div>
+      <div class="name">
+        <span class="firstName">Alexandros</span>
+        <br />
+        <span class="lastName">Efthymiou</span>
+      </div>
       <div class="description">
         Graphics Designer
-        <span style="color: var(--MainText)">-</span> Concept Artist
+        <span>-</span> Concept Artist
       </div>
     </div>
     <div data-icon="border" class="border rightBorder"></div>
@@ -60,9 +64,30 @@ export default {};
   font-size: 7rem;
 }
 
+.firstName {
+  background-color: var(--RedColor);
+  margin-right: 5rem;
+}
+
+.lastName {
+  background-color: var(--LightColor);
+  margin-left: 5rem;
+}
+
+.firstName,
+.lastName {
+  padding: 0 0.2em;
+  color: var(--DarkColor);
+}
+
 .description {
-  color: var(--LightColor);
+  color: var(--DarkColor);
   font-size: 1.2rem;
+  background-color: var(--MainText);
+  width: fit-content;
+  width: -moz-fit-content;
+  padding: 0 0.5rem;
+  float: right;
 }
 
 .artistName {
@@ -78,4 +103,4 @@ export default {};
   margin-top: -8rem;
   margin-left: 15rem;
 }
-</style>>
+</style>
